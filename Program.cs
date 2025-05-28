da830088837d613f463e6ead726b241c63932e72
Mykola Serdiuk <52344283+Rolika4@users.noreply.github.com>
2025-05-28 20:24:10 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/api/hello", () => "Hello, EDP!");
+app.MapMethods("/api/hello", new[] { "HEAD" }, () => Results.Ok());
 
 app.Run();
